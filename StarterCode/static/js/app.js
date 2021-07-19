82d089de553e25ab7aadeedad9c5544cf2c6b953
gary castillo <garycastle@garys-MacBook-Pro.local>
2021-07-19 15:00:11 -0700

    function2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,3 +9,17 @@ function buildMetadata(sample) {
     Object.entries(result).forEach(([key, value]) => {
       panel.append("h6").text(`${key}: ${value}`);
     });
+
+
+function buildCharts(sample) {
+
+
+d3.json("samples.json").then((data) => {
+  var samples= data.samples;
+  var resultsarray= samples.filter(sampleobject => 
+      sampleobject.id == sample);
+  var result= resultsarray[0]
+
+  var ids = result.otu_ids;
+  var labels = result.otu_labels;
+  var values = result.sample_values;
